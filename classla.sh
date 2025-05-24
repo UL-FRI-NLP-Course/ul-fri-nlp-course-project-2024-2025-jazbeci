f10b25698d81d3db2c7c3fcea66db94bbc18c05d
Nina Sangawa Hmeljak <nina.sangawa@gmail.com>
2025-05-24 12:48:50 +0200

    more cpus

--- classla.sh
+++ classla.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=logs/gams-%j.out
 #SBATCH --error=logs/gams-%j.err
 #SBATCH --mem=128G
-#SBATCH --cpus-per-task=3
+#SBATCH --cpus-per-task=6
 #SBATCH --time=12:00:00
 
 singularity exec --nv ../containers/singularity_python3.10_classla.sif bash -c "
