ff380e5657cb8636102763491f6d8cc3d23b64dc
Nina Sangawa Hmeljak <nina.sangawa@gmail.com>
2025-05-28 15:37:39 +0200

    cosine similarity without lemmatization

--- inout_cos_sim/cosine_similarity.py
+++ inout_cos_sim/cosine_similarity.py
@@ -23,7 +23,7 @@ def read_csv_file(file_path):
         # exit the program if the file cannot be read
         sys.exit(f"Failed to read CSV file {file_path}: {e}")
 
-def process_pair(lem_in, lem_out, lem_in_ner, lem_out_ner):
+def process_lem_pair(lem_in, lem_out, lem_in_ner, lem_out_ner):
     cos_sim = pairwise_similarity(lem_in, lem_out)
     if lem_in_ner.strip() and lem_out_ner.strip():
         try:
@@ -33,10 +33,15 @@ def process_pair(lem_in, lem_out, lem_in_ner, lem_out_ner):
     else:
         cos_sim_ner = float('nan')
     return {
-        'CosineSimilarity': cos_sim,
-        'CosineSimilarityNER': cos_sim_ner
+        'LemCosineSimilarity': cos_sim,
+        'LemCosineSimilarityNER': cos_sim_ner
     }
 
+def process_pair(input_text, output_text):
+    cos_sim = pairwise_similarity(input_text, output_text)
+    return {
+        'CosineSimilarity': cos_sim
+    }
 
 def process_and_save(lemmatized_file_name, file_name):
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
@@ -48,16 +53,20 @@ def process_and_save(lemmatized_file_name, file_name):
         df = df.reset_index(drop=True)  # Reset index to ensure proper indexing after reading CSV
         # df has columns 'Datum', 'FilePath', 'InputText', 'OutputText', 'Input', 'Output', 'lem_in', 'lem_out', 'lem_in_ner', 'lem_out_ner'
         df['CosineSimilarity'] = 0.0
-        df['CosineSimilarityNER'] = 0.0
-        pairs_list = df[['lem_in', 'lem_out', 'lem_in_ner', 'lem_out_ner']].values.tolist()
+        # df['CosineSimilarityNER'] = 0.0
+        # pairs_list = df[['lem_in', 'lem_out', 'lem_in_ner', 'lem_out_ner']].values.tolist()
+
+        # df has columns 'Datum', 'FilePath', 'InputText', 'OutputText', 'Input', 'Output', 'IncludedInputKeys'
+        pairs_list = df[['InputText', 'OutputText']].values.tolist()
         with ProcessPoolExecutor(max_workers=6) as executor:
-            futures = {executor.submit(process_pair, lem_in, lem_out, lem_in_ner, lem_out_ner): idx for idx, (lem_in, lem_out, lem_in_ner, lem_out_ner) in enumerate(pairs_list)}
+            # futures = {executor.submit(process_pair, lem_in, lem_out, lem_in_ner, lem_out_ner): idx for idx, (lem_in, lem_out, lem_in_ner, lem_out_ner) in enumerate(pairs_list)}
+            futures = {executor.submit(process_pair, input_text, output_text): idx for idx, (input_text, output_text) in enumerate(pairs_list)}
             for future in as_completed(futures):
                 idx = futures[future]
                 try:
                     result = future.result()
                     df.at[idx, 'CosineSimilarity'] = result['CosineSimilarity']
-                    df.at[idx, 'CosineSimilarityNER'] = result['CosineSimilarityNER']
+                    # df.at[idx, 'CosineSimilarityNER'] = result['CosineSimilarityNER']
                 except Exception as e:
                     logging.error(f"Error processing pair at index {idx}: {e}")
         df.to_csv(file_name, index=False)
@@ -70,7 +79,8 @@ if len(sys.argv) > 1:
 else:
     year = '2024'
 
-input_file = f'./lemmatized_pairs_{year}.csv'
+# input_file = f'./lemmatized_pairs_{year}.csv'
+input_file = f'./pairs_{year}.csv'
 output_file = f'./cos_sim_{year}.csv'
 
 process_and_save(input_file, output_file)
